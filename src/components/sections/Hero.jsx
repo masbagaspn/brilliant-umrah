20165a6ad78e1232d8aa3ba24cbcac0a3e76ad5e
masbagaspn <masbagaspn@gmail.com>
2023-09-06 12:59:37 +0700

    fix: compress hero section iamge

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <section
       className={clsx(
         "relative w-full h-screen overflow-hidden",
-        "bg-[url('/assets/images/section-1-mobile.jpg')] bg-cover bg-top bg-no-repeat",
+        "bg-[url('/assets/images/section-1-mobile.webp')] bg-cover bg-top bg-no-repeat",
         "lg:bg-[url('/assets/images/section-1.webp')]"
       )}
     >
@@ -23,28 +23,6 @@ const Hero = () => {
       >
         Pelajari Lebih Lanjut
       </a>
-      {/* <motion.div
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: loading ? 0 : 1, y: loading ? 200 : 0 }}
-        transition={{ duration: 0.5, delay: 0.25 }}
-        className="w-full h-fit flex flex-col items-center justify-center z-10 text-white gap-6 px-4"
-      >
-        <h1 className="sr-only">Brilliant Umrah</h1>
-        <img
-          src="/assets/images/logo/logo-briliant.png"
-          alt="logo"
-          className="w-16 h-auto object-contain"
-          onLoad={() => setLoading(false)}
-        />
-        <h2 className="section-title text-center">
-          Wujudkan Umrah Impianmu & <br className="hidden lg:block" /> Gapai
-          Baitullah Bersama Brilliant
-        </h2>
-        <p className="paragraph text-white/70 text-center">
-          Allah tidak memanggil orang yang mampu, <br />
-          tetapi Allah memampukan orang yang terpanggil.
-        </p>
-      </motion.div> */}
     </section>
   );
 };
